fbe392c320a94d94069087b8ac254f915207c80e
Swan Htet Aung <swanhtetaung308@gmail.com>
2025-08-16 17:15:26 +0700

    add function

--- rps.py
+++ rps.py
@@ -2,44 +2,52 @@ import sys
 import random
 from enum import Enum
 
-class RPS(Enum):
-    ROCK = 1
-    PAPER = 2
-    SCISSORS = 3
+def play_rps() :
 
-play_again = True
+    class RPS(Enum):
+        ROCK = 1
+        PAPER = 2
+        SCISSORS = 3
 
-while play_again:
 
-    user_input = int(input("\nPlease Enter \n 1 for ROCK, \n 2 for PAPER, or \n 3 for SCISSORS: \n "))
+    user_input = input("\nPlease Enter \n 1 for ROCK, \n 2 for PAPER, or \n 3 for SCISSORS: \n ")
 
-    if user_input < 1 or user_input > 3:
+    if user_input not in ['1', '2', '3']:
         print("Invalid input. Please enter a number between 1 and 3.")
-        sys.exit(1)
+        return play_rps()
 
+    user_choice = int(user_input)
     computer_input = int(random.choice("123"))
 
-    print('\nYour choice: ' + str(RPS(user_input)).replace('RPS.', '') + '.\n')
+    print('\nYour choice: ' + str(RPS(user_choice)).replace('RPS.', '') + '.\n')
     print('Computer choice: ' + str(RPS(computer_input)).replace('RPS.', '') + '.\n')
 
-    if user_input == computer_input:
+    if user_choice == computer_input:
         print('It\'s a tie!')
-    elif user_input == 1 and computer_input == 3:
+    elif user_choice == 1 and computer_input == 3:
         print('You win! ROCK beats SCISSORS.')
-    elif user_input == 2 and computer_input == 1:
+    elif user_choice == 2 and computer_input == 1:
         print('You win! PAPER beats ROCK.')
-    elif user_input == 3 and computer_input == 2:
+    elif user_choice == 3 and computer_input == 2:
         print('You win! SCISSORS beats PAPER.')
     else:
         print('You lose! Better luck next time.')
 
-    play_again = input("\nPlay again? \nY for Yes, \nN for No or \nQ to Quit: \n\n")   
+    print("\nPlay again?")
+
+    while True:
+        play_again = input("\nY for Yes, \nQ to Quit: \n\n")
+        if play_again.lower() not in ['y', 'q']:
+            continue
+        else:
+            break
 
     if play_again.lower() == 'y':
-        continue
+        return play_rps()
     else:
         print("Thanks for playing!")
-        play_again = False
+        sys.exit("Bye Bye!") 
 
-sys.exit("Bye Bye!") 
+
+play_rps()
     
